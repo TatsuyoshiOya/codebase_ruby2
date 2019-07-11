836fe5860c8daa110243cc77f92371082c8ce7c7
TatsuyoshiOya <Kozanchu8131@gmail.com>
2019-07-11 21:04:28 +0900

    条件分岐をした

--- index.rb
+++ index.rb
@@ -1,3 +1,13 @@
 words = 1 + 2
 puts words
 puts 1 + 2
+
+num = gets.to_i
+if num >= 10
+    puts "10以上の数字です"
+    if num % 2 == 0
+        puts "また、偶数でもあります"
+    end
+else 
+    puts "10未満の数字です"
+end
