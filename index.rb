7738285c0bd74c26ff811d60496537e7b883196c
TatsuyoshiOya <Kozanchu8131@gmail.com>
2019-07-11 22:04:11 +0900

    関数の詳しい説明

--- index.rb
+++ index.rb
@@ -1,24 +1,12 @@
-sum = 0
-(1..gets.to_i).each do |num|
-    sum += num
+def sum_up_to(last_number)
+    sum = 0
+    (1..last_number)/each do |num|
+        if num % 2 == 0
+            num += sum
+        end
+    return sum
 end
-puts sum
 
 input = gets.to_i
-if num % 2 == 0
-    (1..input).each do |num|
-        sum += num
-    end 
-end
 
-
-# 答え
-sum = 0
-input = gets.to_i
-(1..input).each do |num| # ヒント: この行のどこかをinputにする
-  # 2のヒント: この↓のコードを、条件分岐の中に入れてあげる(num % 2 == 0)...？
-  if num % 2 == 0
-    sum += num
-  end
-end
-puts sum
+puts sum_up_to(input)
