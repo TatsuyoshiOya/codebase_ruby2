6999c015b2098a28ccb3903a03f4bf21950fd7c0
TatsuyoshiOya <Kozanchu8131@gmail.com>
2019-07-11 21:38:26 +0900

    ループ文演習2までの回答

--- index.rb
+++ index.rb
@@ -1,6 +1,24 @@
 sum = 0
-(1..10).each do |num|
-    puts sum
+(1..gets.to_i).each do |num|
     sum += num
+end
+puts sum
+
+input = gets.to_i
+if num % 2 == 0
+    (1..input).each do |num|
+        sum += num
+    end 
+end
+
+
+# 答え
+sum = 0
+input = gets.to_i
+(1..input).each do |num| # ヒント: この行のどこかをinputにする
+  # 2のヒント: この↓のコードを、条件分岐の中に入れてあげる(num % 2 == 0)...？
+  if num % 2 == 0
+    sum += num
+  end
 end
 puts sum
